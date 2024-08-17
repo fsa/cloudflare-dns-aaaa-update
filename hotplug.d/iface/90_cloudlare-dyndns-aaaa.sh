cea93c1f14b6293c48c4b50095dc7acf1a779a10
Sergei Fedotov <fsa@tavda.info>
2024-08-17 17:02:36 +0700

    Черновик скрипта hotplug для OpenWRT

---
+++ hotplug.d/iface/90_cloudlare-dyndns-aaaa.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+INTERFACE=br-lan
+
+DOMAIN=example.org
+RECORD=my_router.example.org
+RECORD_VALUE=:1
+
+API_TOKEN=CLOUDFLARE_API_TOKEN
+ZONE_ID=ZONE_ID_STEP1
+RECORD_ID=RECORD_ID_STEP2
+
+API_URL="https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID"
+
+if [ "$ACTION" = "ifup" ] && [ "$INTERFACE" = "pppoe-wan" ]; then
+    IPV6_PREFIX=`ip addr show primary -deprecated -mngtmpaddr to 2000::/3 dev $INTERFACE | grep inet6 | head -n 1 | tr -s ' ' '\t' | cut -f3 | cut -d'/' -f1 | cut -d':' -f1-4`
+    NEW_IPV6=${IPV6_PREFIX}:${RECORD_VALUE}
+    $(curl -X PUT "$API_URL" \
+     -H "Authorization: Bearer $API_TOKEN" \
+     -H "Content-Type: application/json" \
+     --data '{
+       "type": "AAAA",
+       "name": "'"$RECORD"'",
+       "content": "'"$NEW_IPV6"'",
+       "ttl": 120,
+       "proxied": false
+     }')
+fi
