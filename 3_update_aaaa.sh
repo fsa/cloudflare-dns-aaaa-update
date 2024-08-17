5034c4b8bafcce30211a3de22a94d217cf35a5ff
Sergei Fedotov <fsa@tavda.info>
2024-08-17 16:15:38 +0700

    Проверка на изменение префикса

--- 3_update_aaaa.sh
+++ 3_update_aaaa.sh
@@ -3,6 +3,12 @@
 if [ -f ".env" ]; then source .env; else exit 1; fi
 
 IPV6_PREFIX=`ip -6 addr show scope global primary -deprecated -mngtmpaddr to 2000::/3 | grep -oP '(?<=inet6\s)([0-9a-f]{1,4}:){4}' | sort -u | head -n 1`
+
+if [ -f old_prefix.txt ]; then
+    OLD_PREFIX=`cat old_prefix.txt`
+    if [ "$IPV6_PREFIX" == "$OLD_PREFIX" ]; then echo "Адрес не изменился."; exit; fi
+fi
+
 NEW_IPV6=${IPV6_PREFIX}:${RECORD_VALUE}
 
 echo "Новый адрес: $NEW_IPV6"
@@ -25,6 +31,7 @@ RESPONSE=$(curl -X PUT "$API_URL" \
 # Проверка ответа API
 if echo "$RESPONSE" | grep -q '"success":true'; then
   echo "Запись AAAA успешно обновлена."
+  echo "$IPV6_PREFIX" > old_prefix.txt
 else
   echo "Не удалось обновить запись AAAA. Ответ API:"
   echo "$RESPONSE"
